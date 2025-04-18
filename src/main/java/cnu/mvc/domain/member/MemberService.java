c612365cf13dec6be2ce63e040c669e1d32582be
202102711JiHyunWoo <105435641+202102711JiHyunWoo@users.noreply.github.com>
2025-04-18 14:35:09 +0900

    Update MemberService.java

--- src/main/java/cnu/mvc/domain/member/MemberService.java
+++ src/main/java/cnu/mvc/domain/member/MemberService.java
@@ -10,11 +10,16 @@ public class MemberService {
     private final MemberRepository memberRepository;
 
     public Member join(Member member){
+        // 이메일 중복 구현
+        if(findByEmail(member.getEmail()) != null ) {
+            throw new IllegalStateException("이미 존재하는 이메일 계정입니다.");
+        }
         return memberRepository.save(member);
     }
 
     public Member validateMember(String email, String pwd) {
         Member findMember = findById(1L);
+        
         return findMember;
     }
 
@@ -25,6 +30,7 @@ public class MemberService {
     // 구현
     public Member findByEmail(String email) {
         return memberRepository.findByEmail(email);
+        
     }
 
 }
